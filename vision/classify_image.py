7c3bd6c186449deacbb871edf6e010a93207c578
Javier (HP Ubuntu) <javierurquizu@gmail.com>
2017-05-03 17:46:42 -0700

    Added calcClassifierAccuracy function

--- vision/classify_image.py
+++ vision/classify_image.py
@@ -16,25 +16,37 @@ RETRAIN_GRAPH = "../tf_classifier/retrained_graph_50000.pb"
 
 
 
-def getImageSet(image_set_path):
+def getImageSet(query_path, pathType):
     """
     Gets a dictionary containing all the images in the given path.
     Includes the imageClass and imagePath.
     """
-    image_set = {}
-    classes = os.listdir(image_set_path)
+    if pathType not in ["image_set", "image"]:
+        raise ValueError("%s is not a correct pathType!" % pathType)
     
-    for testClass in classes:
-        for img in os.listdir(image_set_path +testClass):
-            imgInfo = { 'class' : testClass.lower(), 'path' : (image_set_path +testClass +"/" +img) }
-            image_set[img.split(".")[0]] = imgInfo
+    image_set_info = {}    
+    if pathType == "image_set":
+        classes = os.listdir(query_path)        
+        for testClass in classes:
+            for img in os.listdir(query_path +testClass):
+                imgInfo = { 'class' : testClass.lower(), 'path' : (query_path +testClass +"/" +img) }
+                image_set_info[img.split(".")[0]] = imgInfo
+    
+    # Parse path to return a dictionary with 1 key
+    else: 
+        path_split = query_path.split("/")
+        imgInfo = { 'class':path_split[-2].lower(), 'path':query_path }
+        image_set_info[path_split[-1].split(".")[0]] = imgInfo
+    
+    return image_set_info
+        
 
-    return image_set
-
-
-def classifyImage(retrain_graph, retrain_labels, query_path, pathType="image_set", printInfo=True):
+def classifyImages(retrain_graph, retrain_labels, query_path, pathType="image_set"):
     """
     """
+    if pathType not in ["image_set", "image"]:
+        raise ValueError("%s is not a correct pathType!" % pathType)
+        
     # Loads label file & strips off '/r'
     class_labels = [line.rstrip() for line in tf.gfile.GFile(retrain_labels)]
     
@@ -43,41 +55,70 @@ def classifyImage(retrain_graph, retrain_labels, query_path, pathType="image_set
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
+
+    # Get image set information
+    image_set_info = getImageSet(query_path, pathType)
         
     # Get prediction/classification info
-    with tf.Session() as sess:
-        # Read in the test image
-        testImg = tf.gfile.FastGFile(query_path, 'rb').read()
-        
-        # Feed the test image as input to the graph and get 1st prediction
-        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')        
-        predicts = sess.run(softmax_tensor, {'DecodeJpeg/contents:0':testImg} )
-        
-        # Sort labels of 1st prediction in order of confidence
-        top_k = predicts[0].argsort()[-len(predicts[0]):][::-1]
-    sess.close()
-    
-    # Parse classification info
-    classifyInfo = []
-    for i in top_k:
-        classifyInfo.append({ 'score' : predicts[0][i], 'label' : class_labels[i] })
-    
-    # Display information
-    if printInfo:
-        print('\nUsing graph model: "%s"' % retrain_graph)
-        print('\nImage:\t"%s"' % query_path.split("/")[-1] )
-        for info in classifyInfo:
-            print( '%s\t(score = %.5f)' % (info['label'], info['score']) )
+    with tf.Session() as sess: 
+        for key in sorted(image_set_info.keys()):
+            img_path = image_set_info[key]['path']
+            img = tf.gfile.FastGFile(img_path, 'rb').read()
+            print("Classifying: %s" % img_path.split("/")[-1])
             
-    return classifyInfo
+            # Feed the test image as input to the graph and get 1st prediction
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')        
+            predicts = sess.run(softmax_tensor, {'DecodeJpeg/contents:0':img} )
+            
+            # Sort labels of 1st prediction in order of confidence
+            top_k = predicts[0].argsort()[-len(predicts[0]):][::-1]
+            
+            # Parse classification info
+            classInfo = []
+            for i in top_k:
+                classInfo.append({ 'score' : predicts[0][i], 'label' : class_labels[i] })
+                
+            image_set_info[key]['classInfo'] = classInfo     
+    
+    sess.close()    
+    return image_set_info
+   
 
-'''
-def classifyImageSet(retrain_graph, retrain_labels, set_path, printInfo=True):
-'''     
-
-def calcImageSetAccuracy(printInfo=True):
-    pass
-
+def calcClassifierAccuracy(image_set_info, printInfo=True):
+    """
+    """
+    classifierStats = {}
+    incorrectImages = []
+    totalCorrect = 0
+    totalScore = 0.0
+    
+    if printInfo:
+        print("\nClassifier Stats:")
+    for key in sorted(image_set_info.keys()):
+        gnd_label = image_set_info[key]['class']
+        pred_label = image_set_info[key]['classInfo'][0]['label']
+        score = image_set_info[key]['classInfo'][0]['score']
+        
+        if gnd_label == pred_label:
+            totalCorrect = totalCorrect +1
+            totalScore = totalScore +score
+        else:
+            incorrectImages.append(key)
+            
+        if printInfo:
+            print('\nImage:\t"%s"' % key )
+            for info in image_set_info[key]['classInfo']:
+                print( '%s\t(score = %.5f)' % (info['label'], info['score']) )
+            
+            
+    accuracy = float(totalCorrect)/len(image_set_info.keys())
+    avgScore = totalScore / len(image_set_info.keys())
+    
+    classifierStats['accuracy'] = accuracy
+    classifierStats['avgScore'] = avgScore
+    classifierStats['incorrectImages'] = incorrectImages
+    
+    return classifierStats
 
 
 if __name__ == '__main__':
@@ -85,12 +126,7 @@ if __name__ == '__main__':
     for img_path in [TEST_SET_PATH, RETRAIN_LABELS, RETRAIN_GRAPH]:
         if not os.path.exists(img_path):
             sys.exit("%s does not exist!" % img_path)
+
     
-    
-    test_class = "Sprite"
-    test_img = "sprite_logo_3.jpg"
-    test_img_path = TEST_SET_PATH +test_class +"/" +test_img
-    
-    image_classification =  classifyImage(RETRAIN_GRAPH, RETRAIN_LABELS, test_img_path)
-    
-    image_set = getImageSet(TEST_SET_PATH)
+    image_set_info = classifyImages(RETRAIN_GRAPH, RETRAIN_LABELS, TEST_SET_PATH)
+    image_set_stats = calcClassifierAccuracy(image_set_info)
