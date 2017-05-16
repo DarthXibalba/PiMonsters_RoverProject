1abe88febed32ba301b15485365f558a7f110ef5
DarthXibalba <javierurquizu@gmail.com>
2017-05-16 00:31:03 -0700

    Fixed image classifier to detect if query input is a dataset of classes or a JPG image

--- vision/classify_image.py
+++ vision/classify_image.py
@@ -10,43 +10,47 @@ import tensorflow as tf
 
 
 
-TEST_SET_PATH = "../tf_classifier/ImageDataset/Test/"
 RETRAIN_LABELS = "../tf_classifier/retrained_labels.txt"
 RETRAIN_GRAPH = "../tf_classifier/retrained_graph_50000.pb"
 
+TEST_SET_PATH = "../tf_classifier/ImageDataset/Test/"
+# OR
+TEST_IMAGE_PATH = "../tf_classifier/ImageDataset/Demo/pepsi_bottle_1.jpg"
 
 
-def getImageSet(query_path, pathType):
+
+def getImageSet(query_path):
     """
     Gets a dictionary containing all the images in the given path.
     Includes the imageClass and imagePath.
     """
-    if pathType not in ["image_set", "image"]:
-        raise ValueError("%s is not a correct pathType!" % pathType)
+    if (query_path.strip()[-3:].lower() == "jpg"):
+        is_image = True
+    else:
+        is_image = False
     
     image_set_info = {}    
-    if pathType == "image_set":
+
+    # Parse path to return a dictionary with 1 key
+    if (is_image): 
+        path_split = query_path.split("/")
+        imgInfo = { 'class':path_split[-2].lower(), 'path':query_path }
+        image_set_info[path_split[-1].split(".")[0]] = imgInfo
+    
+    # Populate the dictionary with image information in containing folder
+    else:
         classes = os.listdir(query_path)        
         for testClass in classes:
             for img in os.listdir(query_path +testClass):
                 imgInfo = { 'class' : testClass.lower(), 'path' : (query_path +testClass +"/" +img) }
                 image_set_info[img.split(".")[0]] = imgInfo
     
-    # Parse path to return a dictionary with 1 key
-    else: 
-        path_split = query_path.split("/")
-        imgInfo = { 'class':path_split[-2].lower(), 'path':query_path }
-        image_set_info[path_split[-1].split(".")[0]] = imgInfo
-    
     return image_set_info
         
 
-def classifyImages(retrain_graph, retrain_labels, query_path, pathType="image_set"):
+def classifyImages(retrain_graph, retrain_labels, query_path):
     """
     """
-    if pathType not in ["image_set", "image"]:
-        raise ValueError("%s is not a correct pathType!" % pathType)
-        
     # Loads label file & strips off '/r'
     class_labels = [line.rstrip() for line in tf.gfile.GFile(retrain_labels)]
     
@@ -57,14 +61,14 @@ def classifyImages(retrain_graph, retrain_labels, query_path, pathType="image_se
         _ = tf.import_graph_def(graph_def, name='')
 
     # Get image set information
-    image_set_info = getImageSet(query_path, pathType)
+    image_set_info = getImageSet(query_path)
         
     # Get prediction/classification info
     with tf.Session() as sess: 
         for key in sorted(image_set_info.keys()):
             img_path = image_set_info[key]['path']
             img = tf.gfile.FastGFile(img_path, 'rb').read()
-            print("Classifying: %s" % img_path.split("/")[-1])
+            print("\nClassifying: %s" % img_path.split("/")[-1])
             
             # Feed the test image as input to the graph and get 1st prediction
             softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')        
@@ -128,5 +132,6 @@ if __name__ == '__main__':
             sys.exit("%s does not exist!" % img_path)
 
     
-    image_set_info = classifyImages(RETRAIN_GRAPH, RETRAIN_LABELS, TEST_SET_PATH)
+    image_set_info = classifyImages(RETRAIN_GRAPH, RETRAIN_LABELS, TEST_IMAGE_PATH)
     image_set_stats = calcClassifierAccuracy(image_set_info)
+    print("\n")
