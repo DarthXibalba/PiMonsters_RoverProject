cb17405aa4387484ef860f0ede4bae6a0e4befbd
a1xu <a1xu@ucsd.edu>
2017-05-28 11:52:11 -0700

    Updated servo movement

--- movement/simpletest.py
+++ movement/simpletest.py
@@ -21,6 +21,7 @@ pwm = Adafruit_PCA9685.PCA9685()
 
 # Configure min and max servo pulse lengths
 servo_min = 150  # Min pulse length out of 4096
+servo_claw_max = 215
 servo_max = 600  # Max pulse length out of 4096
 
 # Helper function to make setting a servo pulse width simpler.
@@ -37,10 +38,26 @@ def set_servo_pulse(channel, pulse):
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
+#0, 1, 4, 5, 8
+#0 - 415
+
+pwm.set_pwm(0, 0, servo_claw_max)
+pwm.set_pwm(1, 0, 125)
+pwm.set_pwm(4, 0, servo_max)
+pwm.set_pwm(8, 0, 375)
+pwm.set_pwm(5, 0, 290)
+
+#5 - 125 down, 300 up
+
+#print('Moving servo on channel 0, press Ctrl-C to quit...')
+#while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+   # pwm.set_pwm(3, 0, servo_min)
+   # time.sleep(1)
+   # pwm.set_pwm(3, 0, servo_max)
+   # time.sleep(1)
+
+   # pwm.set_pwm(4, 0, servo_min)
+   # time.sleep(1)
+   # pwm.set_pwm(4, 0, servo_max)
+   # time.sleep(1)
