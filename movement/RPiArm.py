78200d93f45d265c8e3f221527d3b9491782372a
a1xu <a1xu@ucsd.edu>
2017-06-03 17:37:50 -0700

    changed move_servo4 ticks

--- movement/RPiArm.py
+++ movement/RPiArm.py
@@ -9,7 +9,7 @@ class RPiArm:
         self.servo1 = RPiServo.RPiServo(0, 125, 525, 300)
         self.servo2 = RPiServo.RPiServo(1, 150, 600, 300)
         self.servo3 = RPiServo.RPiServo(4, 150, 600, 300)
-        self.servo4 = RPiServo.RPiServo(5, 150, 600, 300)
+        self.servo4 = RPiServo.RPiServo(5, 100, 600, 300)
         self.servo5 = RPiServo.RPiServo(8, 150, 600, 300)
         self.servo6 = RPiServo.RPiServo(9, 200, 600, 300)
 
@@ -35,7 +35,7 @@ class RPiArm:
 
     def move_servo4(self):
         # larger number = forward
-        self.servo4.move_servo(150)
+        self.servo4.move_servo(100)
         # if position == "backward"
 
     # def move_servo5(self, position):
