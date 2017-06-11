0aa0caee0a6d23299e1c06d1e1a8e71693566041
a1xu <a1xu@ucsd.edu>
2017-06-11 12:14:16 -0700

    changed min and max ticks for servo 4 and 6

--- movement/RPiArm.py
+++ movement/RPiArm.py
@@ -9,9 +9,11 @@ class RPiArm:
         self.servo1 = RPiServo.RPiServo(0, 125, 525, 525, 0.025)
         self.servo2 = RPiServo.RPiServo(1, 150, 600, 380, 0.025)
         self.servo3 = RPiServo.RPiServo(4, 150, 650, 600, 0.025)
-        self.servo4 = RPiServo.RPiServo(5, 125, 600, 600, 0.15)
+        # self.servo4 = RPiServo.RPiServo(5, 125, 600, 600, 0.15)
+        self.servo4 = RPiServo.RPiServo(5, 100, 700, 600, 0.15)
         self.servo5 = RPiServo.RPiServo(8, 150, 600, 510, 0.15)
-        self.servo6 = RPiServo.RPiServo(9, 200, 600, 390, 0.025)
+        # self.servo6 = RPiServo.RPiServo(9, 200, 600, 390, 0.025)
+        self.servo6 = RPiServo.RPiServo(9, 100, 700, 390, 0.025)
 
     def move_claw(self, position):
         if position == "open":
